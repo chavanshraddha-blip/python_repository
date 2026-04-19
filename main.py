2cf82cfeed11ebabc9a22c6262dfa9d0d1459a67
Shraddha Chavan <chavanshraddha992@gmail.com>
2026-04-19 21:15:48 +0530

    Week 3: Added POST API to trigger Celery task

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from celery_worker import brake_decision
 
 app = FastAPI()
+
+class DistanceInput(BaseModel):
+    distance: int
 
 @app.get("/")
 def home():
     return {"message": "Brake Decision System Running 🚗"}
 
-@app.get("/check/{distance}")
-def check(distance: int):
-    task = brake_decision.delay(distance)
-    return {
-        "task_id": task.id,
-        "status": "Processing"
-    }
+@app.post("/trigger-task")
+def trigger_task(data: DistanceInput):
+    task = brake_decision.delay(data.distance)
+    return {"task_id": task.id}
